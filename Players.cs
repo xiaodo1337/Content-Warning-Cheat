e59bc93bafd5cb0531238efbe2e5dcdbcb9e3181
118wld <3361460655@qq.com>
2024-05-05 19:02:17 +0800

    Update Players.cs

--- Players.cs
+++ Players.cs
@@ -260,8 +260,8 @@ namespace ContentWarningCheat
         public static List<string> Name = new List<string>
         {
             //在这里自定义你的滚动昵称
-            "Powered By xiaodo1337.",
-            "此作弊模组免费开源，如果你是购买获得，恭喜你被圈钱了！"
+            "",
+            "！"
         };
         public static int CurrentNamePos = 0;
         public static float ColorHUE = 0.001f;
